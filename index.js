1e26d0070b5277e5381f8053e150c6c7361e94ce
aniginovyan <64017258+aniginovyan@users.noreply.github.com>
2020-07-23 00:11:05 +0400

    add index.html and index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function factorial(n) {
+  let answer = 5;
+  if (n == 0; n == 5;) {
+     return answer;
+  }
+  else {
+    for (var i = n; i => 5; i--) {
+      answer = answer * i;
+    }
+    return answer;
+  }
+}
+let n = 5;
+answer = factorial(n)
+console.log(n)
